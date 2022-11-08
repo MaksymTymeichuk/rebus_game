7b8697beb422ff07786f729935d8c82ebe3fc422
sofiaso <91011159+sofiaso@users.noreply.github.com>
2022-11-08 12:38:15 +0200

    some changes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let entry = document.getElementById("task1"),
+ counter = 0,
+ res = document.getElementById("result"),
+ img_arr = ["task1.png","rebus.png","rebus2.png"],
+ img = document.getElementById("img")
+document.getElementById("button").addEventListener("click",()=>
+{
+    if(entry.value == "github" && counter < 3){
+        counter+=1
+        res.innerHTML = "Ваш результат: "+counter
+        img.src = img_arr[counter]
+        //res.style.display ="none"
+    }
+    
+    else if(counter < 1){
+        alert("Неправильно")
+    }
 
+})
